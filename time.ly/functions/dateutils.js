bc6906cbc54ed96d945d2804d07711d4194977e3
Zaiyang Li <zl4917@my.bristol.ac.uk>
2019-03-08 18:41:24 +0000

    todo list api

--- time.ly/functions/dateutils.js
+++ time.ly/functions/dateutils.js
@@ -1,13 +1,17 @@
 
-exports.todaysDateRange = function todaysDateRange() {
-    d1 = new Date()
+exports.dateRangeForDate = function dateRangeForDate(d) {
+    d1 = new Date(d)
     d1.setHours(0,0,0,0)
-    d2 = new Date()
+    d2 = new Date(d)
     d2.setHours(23,59,59,999)
     return {
         startDate: d1,
         endDate: d2,
     }
+}
+
+exports.todaysDateRange = function todaysDateRange() {
+    return dateRangeForDate(new Date())
 }
 
 exports.getMonday = function getMonday(d) {
@@ -27,4 +31,8 @@ exports.thisWeeksDateRange = function thisWeeksDateRange() {
         startDate: d1,
         endDate: d2,
     }
+}
+
+exports.yyyy_mm_dd = function (date) {
+    return date.toISOString().split('T')[0]
 }
